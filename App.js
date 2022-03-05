c21ad0604d60e4d429f396720ef364bb3795849d
studioMac-189 <studiomac-189@studioMac-189s-iMac.local>
2022-03-04 17:45:30 -0800

    3rd commit

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
 import React, { useSate } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { useScreens } from 'react-native-screens';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
 import mealsReducer from './store/reducers/meals';
-//useScreens();
+
 const rootReducer = combineReducers({
   meals: mealsReducer    
 });
